0d2880b39a291a8230cbbc7a07c2e3ea22b1313e
webdacjs <juan@jobspotting.com>
2018-12-19 16:03:49 +0100

    Adding a fetch extract functionality.

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 const fetch = require('node-fetch')
-const wikiprefix = 'https://en.wikipedia.org/w/api.php?action=query'
-const wikiSearchUrl = `${wikiprefix}&list=search&utf8=&format=json&srsearch=`
-const wikiGeoUrl = `${wikiprefix}&prop=coordinates&utf8=&format=json&titles=`
-
-const extractFetchGeo = hits =>
-    hits.length === 0 ? Promise.resolve([]) : fetchGeo(hits[0].title)
-
-const fetchSearch = location =>
-    fetch(`${wikiSearchUrl}${location}`)
-        .then(res => res.json())
-        .then(res => extractFetchGeo(res.query.search))
+const wikiprefix = 'https://en.wikipedia.org/w/api.php?action=query&format=json'
+const wikiSearchUrl = `${wikiprefix}&list=search&utf8=&srsearch=`
+const wikiGeoUrl = `${wikiprefix}&prop=coordinates&utf8=&titles=`
+const wikiExtractUrl = `${wikiprefix}&prop=extracts&exintro&explaintext&redirects=1&titles=`
 
 const fetchGeo = location =>
     fetch(`${wikiGeoUrl}${location}`)
         .then(res => res.json())
         .then(res => res.query.pages[Object.keys(res.query.pages)[0]])
 
+const fetchExtract = query =>
+    fetch(`${wikiExtractUrl}${query}`)
+        .then(res => res.json())
+        .then(res => res.query.pages[Object.keys(res.query.pages)[0]])
+
+const extractFetch = (hits, fn) =>
+    hits.length === 0 ? Promise.resolve([]) : fn(hits[0].title)
+
+const fetchSearchGeo = location =>
+    fetch(`${wikiSearchUrl}${location}`)
+        .then(res => res.json())
+        .then(res => extractFetch(res.query.search, fetchGeo))
+
+const fetchSearchExtract = query =>
+    fetch(`${wikiSearchUrl}${query}`)
+        .then(res => res.json())
+        .then(res => extractFetch(res.query.search, fetchExtract))
+
 module.exports = {
-    fetchSearch,
-    fetchGeo
+    fetchGeo,
+    fetchExtract,
+    fetchSearchGeo,
+    fetchSearchExtract
 }
