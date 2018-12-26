c43c2b2ab7d29fae708fe2e944baffb83e6db9e9
webdac <webdac@gmail.com>
2018-12-26 05:39:46 +0100

    Switching from node fetch to wikifetch in index.js

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-const fetch = require('node-fetch')
-const wikiprefix = 'https://en.wikipedia.org/w/api.php?action=query&format=json'
-const wikiSearchUrl = `${wikiprefix}&list=search&utf8=&srsearch=`
-const wikiGeoUrl = `${wikiprefix}&prop=coordinates&utf8=&titles=`
-const wikiExtractUrl = `${wikiprefix}&prop=extracts&exintro&explaintext&redirects=1&titles=`
+const wikifetch = require('./wikifetch')
+const wikiSearchUrl = '&list=search&utf8=&srsearch='
+const wikiGeoUrl = '&prop=coordinates&utf8=&titles='
+const wikiExtractUrl = '&prop=extracts&exintro&explaintext&redirects=1&titles='
 
 const fetchGeo = location =>
-    fetch(`${wikiGeoUrl}${encodeURIComponent(location)}`)
-        .then(res => res.json())
+    wikifetch(wikiGeoUrl, encodeURIComponent(location))
         .then(res => res.query.pages[Object.keys(res.query.pages)[0]])
 
 const fetchExtract = query =>
-    fetch(`${wikiExtractUrl}${encodeURIComponent(query)}`)
-        .then(res => res.json())
+    wikifetch(wikiExtractUrl, encodeURIComponent(query))
         .then(res => res.query.pages[Object.keys(res.query.pages)[0]])
 
 const extractFetch = (hits, fn) =>
     hits.length === 0 ? Promise.resolve([]) : fn(hits[0].title)
 
 const fetchSearchGeo = location =>
-    fetch(`${wikiSearchUrl}${location}`)
-        .then(res => res.json())
+    wikifetch(wikiSearchUrl, location)
         .then(res => extractFetch(res.query.search, fetchGeo))
 
 const fetchSearchExtract = query =>
-    fetch(`${wikiSearchUrl}${query}`)
-        .then(res => res.json())
+    wikifetch(wikiSearchUrl, query)
         .then(res => extractFetch(res.query.search, fetchExtract))
 
 module.exports = {
-    fetchGeo,
-    fetchExtract,
-    fetchSearchGeo,
-    fetchSearchExtract
+  fetchGeo,
+  fetchExtract,
+  fetchSearchGeo,
+  fetchSearchExtract
 }
