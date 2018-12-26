a4f28d1c89d2880aef6707948c93aefd9c49bed9
webdac <webdac@gmail.com>
2018-12-26 06:43:52 +0100

    Adding fetchGeo and fetchSearch tests.

---
+++ index.test.js
@@ -0,0 +1,17 @@
+const {fetchGeo, fetchSearchGeo} = require('./index')
+
+test(`'fetchGeo' dublin to be equal to 53.x, -6.x`, () => {
+  return fetchGeo('dublin').then(res => {
+    const cooordinates = res.coordinates[0] || {}
+    expect(parseInt(cooordinates.lat)).toBe(53)
+    expect(parseInt(cooordinates.lon)).toBe(-6)
+  })
+})
+
+test(`'fetchSearchGeo' cartagena, colombia to be equal to 53.x, -6.x`, () => {
+  return fetchSearchGeo('cartagena, colombia').then(res => {
+    const cooordinates = res.coordinates[0] || {}
+    expect(parseInt(cooordinates.lat)).toBe(10)
+    expect(parseInt(cooordinates.lon)).toBe(-75)
+  })
+})
