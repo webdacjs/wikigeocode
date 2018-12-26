ec89a892a8bb48fb85d44b985c054cb5acbaad90
webdac <webdac@gmail.com>
2018-12-26 06:46:28 +0100

    Updating tests

--- index.test.js
+++ index.test.js
@@ -8,7 +8,7 @@ test(`'fetchGeo' dublin to be equal to 53.x, -6.x`, () => {
   })
 })
 
-test(`'fetchSearchGeo' cartagena, colombia to be equal to 53.x, -6.x`, () => {
+test(`'fetchSearchGeo' cartagena, colombia to be equal to 10.x, -75.x`, () => {
   return fetchSearchGeo('cartagena, colombia').then(res => {
     const cooordinates = res.coordinates[0] || {}
     expect(parseInt(cooordinates.lat)).toBe(10)
