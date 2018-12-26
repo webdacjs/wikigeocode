5e57ada7a34fc666cb2175e6b3f001ca5a1fe598
webdac <webdac@gmail.com>
2018-12-26 07:07:58 +0100

    Add fetchExtract and fetchSearchExtract tests

--- index.test.js
+++ index.test.js
@@ -1,4 +1,9 @@
-const {fetchGeo, fetchSearchGeo} = require('./index')
+const {
+  fetchGeo,
+  fetchSearchGeo,
+  fetchExtract,
+  fetchSearchExtract
+} = require('./index')
 
 test(`'fetchGeo' dublin to be equal to 53.x, -6.x`, () => {
   return fetchGeo('dublin').then(res => {
@@ -13,5 +18,19 @@ test(`'fetchSearchGeo' cartagena, colombia to be equal to 10.x, -75.x`, () => {
     const cooordinates = res.coordinates[0] || {}
     expect(parseInt(cooordinates.lat)).toBe(10)
     expect(parseInt(cooordinates.lon)).toBe(-75)
+  })
+})
+
+test(`'fetchExtract' dublin to include the irish name in the extract.`, () => {
+  return fetchExtract('dublin').then(res => {
+    const extract = res.extract || {}
+    expect(extract.includes('Baile Átha Cliath')).toBe(true)
+  })
+})
+
+test(`'fetchSearchExtract' cartagena, colombia to include Cartagena de Indias in the extract.`, () => {
+  return fetchSearchExtract('cartagena, colombia').then(res => {
+    const extract = res.extract || {}
+    expect(extract.includes('Cartagena de Indias')).toBe(true)
   })
 })
